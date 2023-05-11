7cceecfa3bb5103377f42c962b14d4dd9e46ef6c
Fernando Cuellar <fercuellar@gmail.com>
2023-05-11 17:13:09 -0600

    variables

---
+++ practicas_random/variable.py
@@ -0,0 +1,30 @@
+# Una variable es un contenedor para un valor, que puede ser de varios tipos
+
+'''
+Este es un comentario multilinea
+o dcostring (usado para documentar una clase o funcion)
+puede ser comilla sencilla o doble.
+'''
+
+"""
+REGLAS DE LA VARIABLES:
+- Los nombres de las variables son case sensitive (diferencia entre mayusculas y minusculas)
+- Deben de empezar con una letra o un guion bajo
+- No pueden empezar con un numero
+- Solo pueden contener caracteres alfanumericos y guion bajo (A-z, 0-9, y _)
+- Los nombres de las variables son case sensitive (diferencia entre mayusculas y minusculas)    
+"""
+
+x = 1           # int
+y = 2.5         # float
+name = 'John'   # str
+is_cool = True  # bool
+
+
+# Multiple asignacion
+
+x, y, name, is_cool = (1, 2.5, 'John', True) # Asignacion multiple
+
+print(x)
+
+
