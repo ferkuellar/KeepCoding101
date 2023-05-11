86a14b2032acba4b2c903882e29f03cf545c7e83
Fernando Cuellar <fercuellar@gmail.com>
2023-05-11 17:17:18 -0600

    fin de variables

--- practicas_random/variable.py
+++ practicas_random/variable.py
@@ -15,16 +15,30 @@ REGLAS DE LA VARIABLES:
 - Los nombres de las variables son case sensitive (diferencia entre mayusculas y minusculas)    
 """
 
-x = 1           # int
-y = 2.5         # float
-name = 'John'   # str
-is_cool = True  # bool
+# x = 1           # int
+# y = 2.5         # float
+# name = 'John'   # str
+# is_cool = True  # bool
 
 
 # Multiple asignacion
 
 x, y, name, is_cool = (1, 2.5, 'John', True) # Asignacion multiple
 
-print(x)
+# Basic math
+
+a = x + y
+
+# Casting
+
+x = str(x)
+y= int(y)
+z = float(y)
+
+
+print(x, y, name, is_cool, a)
+print(type(x))
+print(type(y), y)
+print(type(z), z)
 
 
