8f2ec9fd4acef12b511c40f6c4b261c32eb284e0
Fernando Cuellar <fercuellar@gmail.com>
2023-05-23 19:23:13 -0700

    se explica la funcion print() y se hace un ejemplo de como usarlo

---
+++ practicas_random/print.py
@@ -0,0 +1,8 @@
+# print() es una funcion usada en python 
+# para imprimir en pantalla, print() es una de las funciones mas 
+# usadas en python.
+
+print("hola mundo")
+
+# print() puede imprimir cualquier tipo de dato
+# print() puede ser con "" o ''.
